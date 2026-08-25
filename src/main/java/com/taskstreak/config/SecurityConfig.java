40e7c5f9550994e783ddb70589896aeb1087c058
AbhinavMailaram <abhinavmailaram765@gmail.com>
2026-08-25 20:04:24 +0530

    add capacitor origins to CORS(In SecurityConfig Class)

--- src/main/java/com/taskstreak/config/SecurityConfig.java
+++ src/main/java/com/taskstreak/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("https://task-streak-frontend.vercel.app"));
+        configuration.setAllowedOriginPatterns(List.of("https://task-streak-frontend.vercel.app", "capacitor://localhost","https://localhost"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
         configuration.setAllowCredentials(true);
